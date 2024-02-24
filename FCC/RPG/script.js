164258df69acc903807a74b9990e35369830f436
Nate Garcia <nategarcia.va@gmail.com>
2024-02-24 14:22:36 -0500

    Updates gofight and attack

--- FCC/RPG/script.js
+++ FCC/RPG/script.js
@@ -160,7 +160,10 @@ function goFight() {
 }
 
 function attack() {
-
+  text.innerText = "The " + monsters[fighting].name + " attacks.";
+  text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
+  health -= monsters[fighting].level;
+  monsterHealth -= weapons[currentWeapon].power;
 }
 
 function dodge() {
